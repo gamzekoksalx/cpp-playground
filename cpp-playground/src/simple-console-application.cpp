d6d575d341849bb69b099ce5f9b1d28df9be16ff
gamzekoksalx <gamzekoksalx@gmail.com>
2026-01-18 21:43:18 +0300

    Initial commit

---
+++ cpp-playground/src/simple-console-application.cpp
@@ -0,0 +1,20 @@
+#include <iostream>   // Allows input/output
+using namespace std;
+
+int main() {
+    // Store fictional personal information
+    string firstName = "Harry";
+    string lastName = "Potter";
+    string streetAddress = "4 Privet Drive";
+    string city = "Little Whinging";
+    string zipCode = "CR3 0AA";
+
+    // Display the information to the console
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;  // End of program
+}
